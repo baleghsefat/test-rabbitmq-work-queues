ed62ae6db55e33b4b50819c1789f6294d004a821
mahdibaleghsefat <m.baleghsefat@gmail.com>
2022-01-11 23:19:00 +0330

    [test][rabbitmq][work-queues][Fair dispatch]

--- new_task.php
+++ new_task.php
@@ -25,7 +25,7 @@ $channel->queue_declare('task_queue', false, true, false, false);
 
 $data = implode(' ', array_slice($argv, 1));
 if (empty($data)) {
-    $data = "Hello World!";
+    $data = "Hello... World!";
 }
 
 $msg = new AMQPMessage(
@@ -34,6 +34,13 @@ $msg = new AMQPMessage(
 );
 
 $channel->basic_publish($msg, '', 'task_queue');
+
+// You might have noticed that the dispatching still doesn't work exactly as we want.
+// For example in a situation with two workers, when all odd messages are heavy and even messages
+// are light, one worker will be constantly busy and the other one will do hardly any work. Well,
+// RabbitMQ doesn't know anything about that and will still dispatch messages evenly.
+
+$channel->basic_qos(null, 1, null);
 
 echo ' [x] Sent ', $data, "\n";
 
