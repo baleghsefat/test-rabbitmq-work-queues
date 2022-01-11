13392434f6afef386268a291b0804b24f4bb3f12
mahdibaleghsefat <m.baleghsefat@gmail.com>
2022-01-11 22:36:38 +0330

    [test][rabbitmq][work-queues][Round-robin dispatching]

---
+++ worker.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$callback = function ($msg) {
+    echo ' [x] Received ', $msg->body, "\n";
+    sleep(substr_count($msg->body, '.'));
+    echo " [x] Done\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while ($channel->is_open()) {
+    $channel->wait();
+}
